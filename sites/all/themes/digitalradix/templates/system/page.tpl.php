7d7fa6a5eb6a9857af51bd12c825460f524abb43
EWB <ewilliamsbergen@stlawu.edu>
2014-08-12 13:57:19 -0400

    theme tweeks to remove footer junk

--- sites/all/themes/digitalradix/templates/system/page.tpl.php
+++ sites/all/themes/digitalradix/templates/system/page.tpl.php
@@ -88,10 +88,10 @@
 </div> <!-- /#main-wrapper -->
 
 <footer id="footer" class="footer" role="footer">
-  <div class="container">
+<!--   <div class="container">
     <?php if ($copyright): ?>
       <small class="copyright pull-left"><?php print $copyright; ?></small>
     <?php endif; ?>
     <small class="pull-right"><a href="#"><?php print t('Back to Top'); ?></a></small>
-  </div>
+  </div> -->
 </footer>
