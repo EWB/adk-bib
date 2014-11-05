18d9dac4b22958cb93c49d51845e233ae820aa31
EWB <ewil@stlawu.edu>
2014-11-05 08:58:28 -0500

    updates to logo location

--- sites/all/themes/digital_radix/templates/page/page.tpl.php
+++ sites/all/themes/digital_radix/templates/page/page.tpl.php
@@ -14,7 +14,7 @@
 <div id="top_bar_container">
       <div id="top_bar">
         <div id="slu">
-        <a href="http://www.stlawu.edu"><img id="stlaw" src="./images/slu-horizontal-white.png" alt="St. Lawrence University" height="38"/></a>
+        <a href="http://www.stlawu.edu"><img id="stlaw" src="http://digital.stlawu.edu/adirondack-bibliography/sites/all/themes/digital_radix/images/slu-horizontal-white.png" alt="St. Lawrence University" height="38"/></a>
          </div>
       </div> 
 </div>       
